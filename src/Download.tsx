06b30305a48e2d38db04cf0b12bcfa64a015a7a1
Alkaseem <alkaseemabubakar27@gmail.com>
2021-04-28 10:55:37 +0100

    comitted

--- src/Download.tsx
+++ src/Download.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { PDFDownloadLink } from "@react-pdf/renderer";
-import Pdf from "react-to-pdf";
+import ReactToPdf from "react-to-pdf";
 // import Pdf from "./Pdf";
 import { File } from "react-feather";
 import Demo from "./Demo";
@@ -13,15 +13,17 @@ const Download = () => {
       <div className="post" ref={ref}>
         <h1>Goodmoeni</h1>
       </div>
-      {/* <PDFDownloadLink fileName={`My test`} document={<Demo />}>
+      <PDFDownloadLink fileName={`My test`} document={<Demo />}>
         <button>
           <File size={10} strokeWidth={3} />
           <span className="text-success">Get PDF</span>
         </button>
-      </PDFDownloadLink> */}
-      <Pdf targetRef={ref} filename="test.pdf">
-        {({ toPdf }) => <button onClick={toPdf}>capute</button>}
-      </Pdf>
+      </PDFDownloadLink>
+      {/* <ReactToPdf targetRef={ref} filename="div-blue.pdf">
+        {({toPdf}) => (
+            <button onClick={toPdf}>Generate pdf</button>
+        )}
+    </ReactToPdf> */}
     </>
   );
 };
